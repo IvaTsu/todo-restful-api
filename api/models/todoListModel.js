10b0336eeb8a0809d85b4c2ecaa06185995cfe62
IvaTsu <ivan.tsud@gmail.com>
2018-05-23 19:46:08 +0300

    add Schema with mongoose

---
+++ api/models/todoListModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task',
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed'],
+    }],
+    default: ['pending'],
+  },
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
