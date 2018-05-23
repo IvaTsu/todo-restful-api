c237d117e4caa55687f6139b5c431c2db20c6f76
IvaTsu <ivan.tsud@gmail.com>
2018-05-23 19:33:54 +0300

    add mongoose and fixes in server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000;
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.listen(port);
 
-console.log(`Server is running on: http://localhost:${port}`);
+console.log(`Server is running at: http://localhost:${port}`);
